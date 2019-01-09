0bc2cc968b709f102b7beec794b274fbe092eb0d
Michael Nguyen <thinknownotforever@gmail.com>
2019-01-09 10:13:48 -0800

    All but modal is working

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+var friendsData = [
+    {
+      "name":"Ahmed",
+      "photo":"https://fortunedotcom.files.wordpress.com/2018/06/riz-ahmed.jpg",
+      "scores":[
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+        ]
+    },
+    {
+      "name":"Mr. Anderson",
+      "photo":"https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-matrix-keanu-reeves.jpg",
+      "scores":[
+          "3",
+          "3",
+          "3",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+        ]
+    
+    },
+    {
+      "name":"Agent Smith",
+      "photo":"https://vignette.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest/scale-to-width-down/307?cb=20140504013834",
+      "scores":[
+          "3",
+          "1",
+          "3",
+          "3",
+          "5",
+          "2",
+          "4",
+          "5",
+          "1",
+          "5"
+        ]
+      },
+      {
+        "name":"The Oracle",
+        "photo":"https://vignette.wikia.nocookie.net/matrix/images/c/c9/The_Oracle.jpg/revision/latest/scale-to-width-down/1000?cb=20130119085243",
+        "scores":[
+            "5",
+            "2",
+            "3",
+            "5",
+            "1",
+            "3",
+            "5",
+            "1",
+            "2",
+            "1"
+          ]
+        }
+    
+    ]
+    
+    module.exports = friendsData
+    
